624d05c64beea7d29eceb0af3af7412f73bf6d7b
Jtoscanoa <jtoscanoa.personal@gmail.com>
2024-11-19 18:53:29 -0500

    .

--- src/gestionHumana/Empleado.java
+++ src/gestionHumana/Empleado.java
@@ -1,18 +1,14 @@
-package **;
-
+package gestionHumana; //Nombre de la 'carpeta'
 public class Empleado {
-
-    ** final long cedula;
-    ** String nombre;
-    ** String cargo;
-    
+    public final long cedula; //Necesita ser accedido en otra clase de otro paquete
+    private String nombre; //No necesita ser accedido en otra clase de otro paquete
+    private String cargo; //No necesita ser accedido en otra clase de otro paquete
     public Empleado(long cedula, String nombre, String cargo) {
         this.cedula = cedula;
         this.nombre = nombre;
         this.cargo = cargo;
     }
-
-    ** boolean tengoPermiso() {
+    public boolean tengoPermiso() {  //Necesita ser accedida desde otra clase de otro paquete
         return cargo.contains("Administrador");
     }
 }
