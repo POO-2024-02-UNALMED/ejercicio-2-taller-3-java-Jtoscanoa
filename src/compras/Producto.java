97a271ef1df7c5887b28cfa81cf9515a6069771a
Jtoscanoa <jtoscanoa.personal@gmail.com>
2024-11-19 18:52:19 -0500

    .

--- src/compras/Producto.java
+++ src/compras/Producto.java
@@ -1,31 +1,24 @@
 package compras;
-
 public class Producto {
-    
-    ** final int codigo;
-    ** String nombre;
-    ** String tipo;
-    ** static int totalProductosPedidos;
-
-    ** Producto(int codigo, String nombre, String tipo) {
+    private final int codigo; //No se usa fuera de la clase
+    private String nombre; //No se usa fuera de la clase
+    String tipo;  //Necesito ser accedido en otra clase del mismo paquete
+    static int totalProductosPedidos; //Atributo de clase
+    public Producto(int codigo, String nombre, String tipo) { //Necesito ser accedido en otra clase de otro paquete
         this.codigo = codigo;
         this.nombre = nombre;
         this.tipo = tipo;
     }
-
-    ** void imprimirNombre() {
+    void imprimirNombre() { //Default para ser conocido en todo el paquete
         System.out.print(nombre);
     }
-
     public void setCodigo(int codigo) {
-        **
+        return; //pass
     }
-
-    public ** getCodigo() {
+    public int getCodigo() { //Codigo es de tipo entero
         return codigo;
     }
-
-    ** static int getTotalProductosPedidos() {
+    public static int getTotalProductosPedidos() { //Se necesita accederlo en otra clase de otro paquete
         return totalProductosPedidos;
     }
 }
